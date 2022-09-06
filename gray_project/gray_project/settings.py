df4c5a7e9c4a6deb50d52d26748e2d4104976481
crypticwisdom <crypticwisdom84@gmail.com>
2022-09-06 12:32:42 +0100

    added

--- gray_project/gray_project/settings.py
+++ gray_project/gray_project/settings.py
@@ -11,12 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import django_graylog
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -37,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
+    'django.contrib.messages',import django_heroku
     'django.contrib.staticfiles',
     'app1.apps.App1Config',
 ]
