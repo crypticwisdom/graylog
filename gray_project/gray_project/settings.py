aa93790a1636b119794722bf1089c2dd420059aa
crypticwisdom <crypticwisdom84@gmail.com>
2022-09-06 12:34:53 +0100

    added

--- gray_project/gray_project/settings.py
+++ gray_project/gray_project/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',import django_heroku
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'app1.apps.App1Config',
 ]
